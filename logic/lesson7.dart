4769c27d5f12760e896032d40105821387a1899b
mishael254 <mishaelmishantoz@gmail.com>
2024-07-18 12:08:58 +0300

    finish the logic learning

--- logic/lesson7.dart
+++ logic/lesson7.dart
@@ -1,3 +1,27 @@
-void main (){
-  
+void main() {
+  /**
+   * if 
+   * if else
+   * if else if 
+   * 
+   */
+
+  // if statement
+  int myNum = 6;
+  if (myNum == 5) {
+    print("my number is equal to five\n");
+  } else {
+    print("my number is not five it is $myNum\n");
+  }
+
+  // if else if
+
+  if (myNum <= 0) {
+    print("my number is greater than 0");
+  } else if (myNum <= 5 && myNum >= 6) {
+    print("my number is less than 5");
+  } else {
+    print(
+        "my number is neither greater than zero or less than five it is $myNum");
+  }
 }
