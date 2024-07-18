6571734d90228010825e6d2c9eedbf63a5469bd3
mishael254 <mishaelmishantoz@gmail.com>
2024-07-18 11:51:44 +0300

    learn of loops that is the while loop for loop and the for in loop

--- loops/lesson6.dart
+++ loops/lesson6.dart
@@ -1,3 +1,19 @@
-void main(){
-  
+void main() {
+  // for loop
+  int num = 6;
+  for (int i = num; i >= 0; i--) {
+    print("the value of i is $i\n");
+  }
+
+  // for in loop
+  var names = ["Kirira ", "Vincent", "Taalam"];
+  for (var name in names) {
+    print(name);
+  }
+
+  // while loop
+  while (num > 1) {
+    print("num is $num");
+    num--;
+  }
 }
